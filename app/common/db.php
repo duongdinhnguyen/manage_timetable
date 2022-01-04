6406ed96b401b8b938ab9d32064fe28ddeb04576
duongdinhnguyen <dinhduongco1308@gmail.com>
2022-01-04 12:04:36 +0700

    update handel fix query in search schedule

--- app/common/db.php
+++ app/common/db.php
@@ -18,15 +18,4 @@
 
         }
     }
-    
-	$serverName='localhost';
-	$userName = 'root';
-	$password = '';
-	$dbName = 'manage_timetable';
-	try{
-		$connect = new PDO("mysql:host=$serverName;dbname=$dbName",$userName,$password);
-		$connect -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}catch(PDOException $exception){
-		echo "Connection Failed.". $exception->getMessage();
-	}
 ?>
