e42ddc1c6106f5f8dd27212872f2b6f747f11b6a
DuongHN <91928508+DngHNKz@users.noreply.github.com>
2022-01-05 21:31:07 +0700

    Update search_teacher.php

--- app/views/search_teacher.php
+++ app/views/search_teacher.php
@@ -1,2 +1,87 @@
-<?php
-echo 'Đây là screen tìm kiếm teacher';
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./web/css/base.css">
+    <title>Tìm giáo viên</title>
+</head>
+
+<body>
+  <div class="timetable">
+		<form action="" class="form" method="GET">
+			<div class="main">
+				<div class="element">
+                <?php 
+                echo $_SESSION['search-notification-tc'];
+                ?>
+                </div>
+
+                <div class="element">
+                	<label for="bo-mon">Bộ môn</label>
+	                <select id="bo-mon" class="select-element" name="bo-mon">
+	                    <option value="" name="bo-mon">Chọn bộ môn</option>
+	                    <option value="com-sc" name="bo-mon">Khoa hoc m</option>
+	                    <option value="data-sc" name="bo-mon">Khoa hoc d</option>
+	                    <option value="ocean" name="bo-mon">Hai duong</option>
+	                </select>
+            	</div>
+
+            	<div class="element">
+	                <label for="tu-khoa">Từ khóa</label>
+	                <input type="text" id="tu-khoa" class="input-element" name="keyword">
+            	</div>
+
+            	<div class="element">
+	                <button type="submit" class="btn-submit" name="search-teacher">Tìm kiếm</button>
+	            </div>
+
+				<div class="element">
+                    <?php 
+                    if(!intval($_SESSION['search-notification-tc'])): ?>
+                        <p class="message-error"><?php echo $_SESSION['search-notification-tc']; ?></p>
+                    <?php
+                    else:
+                    ?>
+                        <p class="message-success"><?php echo "Tìm thấy " .$_SESSION['search-notification-tc'] ." giáo viên"; ?></p>
+                    <?php
+                        endif;
+                    ?>    
+                </div>
+
+	            <?php 
+                if($_SESSION['dataSearchTc']): // kiểm tra $_SESSION['dataSearch'] có value hay không? khác với kiểm tra isset
+                ?>
+	                <table>
+	                	<tr>
+	                		<th>ID</th>
+                    		<th>Tên giáo viên</th>
+                    		<th>Bộ môn</th>
+                    		<th>Mô tả</th>
+                    		<th>Action</th>
+	                	</tr>
+
+	                	<?php
+                        foreach($_SESSION['dataSearchTc'] as $row): 
+                        ?>
+                        	<tr>
+                        		<td>
+                    			<button type="submit" name="remove-teacher" class="btn-submit btn-remove" onclick="return confirm('Bạn chắc chắn muốn xóa?');" value = "<?php echo $row['id'];?>">Xóa</button>
+                                <button type="submit" name="change-subject" class="btn-submit btn-change" value = "<?php echo $row['id'];?>">Sửa</button>
+                        		</td>
+                        	</tr>
+                        <?php endforeach; ?>
+	                </table>
+	            <?php endif; ?>
+
+                <div class="element">
+                    <a href="<?= URLROOT ?>/?router=home" >Trở lại trang chủ</a>
+                </div>
+                
+			</div>
+		</form>
+	</div>
+</body>
+</html>
