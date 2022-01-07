b7d571f07b418466030386a5212aff16735d259c
duongdinhnguyen <dinhduongco1308@gmail.com>
2022-01-07 19:50:28 +0700

    'update data lesson, note' in file lesson, note from table schedule and check exists file lesson, note

--- app/controllers/add_schedule_complete.php
+++ app/controllers/add_schedule_complete.php
@@ -16,8 +16,13 @@ class AddScheduleComplete{
         if(isset($_SESSION['data-schedule-update'])  && $_SESSION['data-schedule-update'] !=0){
             // Nếu đúng thì thực hiện update vào data và update vào 2 file note và lesson
             $schedule = $Schedule->searchScheduleFromId($_SESSION['data-schedule-update']);// Lấy ra schedule theo $id
-            $lessonName = $schedule[4];
-            $lessonName = $schedule[5];
+            $lessonNameFile = $schedule[4];
+            $noteNameFile = $schedule[5];
+
+            //update data vào file lesson
+            $this->updateLesson( $lessonNameFile, $data[4]);
+            $this->updateNote( $noteNameFile, $data[5]);
+
             $Schedule->updateSchedule($data, $_SESSION['data-schedule-update']);// đưa dữ liệu cần thay đổi và id để update vào table schedule
             $_SESSION['data-schedule-update'] =0;
 
@@ -35,8 +40,14 @@ class AddScheduleComplete{
 
     // Tạo file chứa thông tin Tiết học và notes
     public function createLessonFile($data){
-        $lessonNameFile = "l". rand(1,99999) .".txt";
-        $path = "./web/file/lesson/". $lessonNameFile;
+        
+        do{
+            //Tạo file chưa thông tin Note
+            $lessonNameFile = "l". rand(1,99999) .".txt";
+            $path = "./web/file/lesson/". $lessonNameFile;
+        }
+        while(file_exists($path));// nếu nó đã tồn tại(trùng với file khác trên database) thì tạo lại
+        
         $fp = @fopen($path, "w+");
         if(!$fp){
             $_SESSION['add-schedule-complete-notifi'] = "Không tạo được file";
@@ -53,8 +64,13 @@ class AddScheduleComplete{
 
     //Tạo file chưa thông tin Note
     public function createNoteFile($data){
-        $noteNameFile = "n". rand(1,99999) .".txt";
-        $path = "./web/file/note/". $noteNameFile;
+        
+        do{
+            // tạo mới một file để chứa note
+            $noteNameFile = "n". rand(1,99999) .".txt";
+            $path = "./web/file/note/". $noteNameFile;
+        }
+        while(file_exists($path));// nếu nó đã tồn tại(trùng với file khác trên database) thì tạo lại
         $fp = @fopen($path, "w+");
         if(!$fp){
             $_SESSION['add-schedule-complete-notifi'] = "Không tạo được file";
@@ -66,6 +82,34 @@ class AddScheduleComplete{
             fclose($fp);
         }
         return $noteNameFile;
+    }
+
+    //update file lesson
+    public function updateLesson($lessonNameFile, $data){
+        $path = "./web/file/lesson/".$lessonNameFile;
+        $fp =@fopen($path, "w");
+        if(!$fp){
+            $_SESSION['add-schedule-complete-notifi'] = "Không mở được file";
+        }
+        else{
+            foreach($data as $row){
+                fwrite($fp, $row."\n");
+            }
+            fclose($fp);
+        }
+    }
+
+    //update file note
+    public function updateNote($noteNameFile, $data){
+        $path = "./web/file/note/".$noteNameFile;
+        $fp =@fopen($path, "w");
+        if(!$fp){
+            $_SESSION['add-schedule-complete-notifi'] = "Không mở được file";
+        }
+        else{
+            fwrite($fp, $data);
+            fclose($fp);
+        }
     }
 }
 
