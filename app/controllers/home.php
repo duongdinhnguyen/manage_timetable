ca656bbccb9d2aaea4184573738a0df4a1f24188
duongdinhnguyen <dinhduongco1308@gmail.com>
2022-01-07 18:24:41 +0700

    fix error change schedule

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,2 +1,8 @@
-<?php 
+<?php
 require_once './app/views/home.php';
+$_SESSION['data-schedule-update']=0; 
+// Khi nhấn sửa schedule thì đã lưu id vào $_SESSION['data-schedule-update']
+// Vì screen sửa schedule dùng chung screen của add-schedule nên đến đoạn nhấn confirm sẽ thực hiện khác nhau
+// check thằng $_SESSION['data-schedule-update'] để quyết định là thêm hay là sửa
+// $_SESSION['data-schedule-update'] chỉ mất sau khi đã sửa xong, nhưng nếu thoát về login hoặc home thì nó vẫn còn nên khi add schedule mới 
+// nó sẽ tính là sửa nên ở home mới refesh $_SESSION['data-schedule-update']=0; 
